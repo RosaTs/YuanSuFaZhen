84972a266901bf2962c189a17be58de3cafc2543
雪狐Monster <1161814672@qq.com>
2018-09-22 17:27:54 +0800

    修改框架

---
+++ Assets/LeanTouch/Examples/Scripts/LeanRotateCustomAxis.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace Lean.Touch
+{
+	// This component allows you to rotate the current GameObject around a specific local axis using finger twists
+	public class LeanRotateCustomAxis : MonoBehaviour
+	{
+		[Tooltip("Ignore fingers with StartedOverGui?")]
+		public bool IgnoreStartedOverGui = true;
+
+		[Tooltip("Ignore fingers with IsOverGui?")]
+		public bool IgnoreIsOverGui;
+
+		[Tooltip("Allows you to force rotation with a specific amount of fingers (0 = any)")]
+		public int RequiredFingerCount;
+
+		[Tooltip("Does rotation require an object to be selected?")]
+		public LeanSelectable RequiredSelectable;
+
+		[Tooltip("The axis of rotation")]
+		public Vector3 Axis = Vector3.down;
+
+		[Tooltip("Rotate locally or globally?")]
+		public Space Space = Space.Self;
+
+#if UNITY_EDITOR
+		protected virtual void Reset()
+		{
+			Start();
+		}
+#endif
+
+		protected virtual void Start()
+		{
+			if (RequiredSelectable == null)
+			{
+				RequiredSelectable = GetComponent<LeanSelectable>();
+			}
+		}
+        float lastAngel = 0;
+		protected virtual void Update()
+		{
+			// Get the fingers we want to use
+			var fingers = LeanSelectable.GetFingersOrClear(IgnoreStartedOverGui, IgnoreIsOverGui, RequiredFingerCount, RequiredSelectable);
+            
+            // Calculate the rotation values based on these fingers
+            var twistDegrees = LeanGesture.GetTwistDegrees(fingers);
+            twistDegrees = Mathf.Lerp(lastAngel, twistDegrees,0.05f);
+            lastAngel = twistDegrees;
+            // Perform rotation
+            transform.Rotate(Axis, twistDegrees, Space);
+		}
+	}
+}
--- Assets/MyScripts/FreamWork/UIType.cs
+++
@@ -1,5 +0,0 @@
-﻿
-enum PanelType
-{
-    BGAlph,      //半透明不可选取下方的按钮
-}
